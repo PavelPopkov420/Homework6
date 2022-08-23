fbfc131d0d9182d184b12576b45b36c9d04f98a9
Pavel Popkov <goil2011@yandex.ru>
2022-08-23 20:43:58 +0300

    task41

---
+++ task41/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите число: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    if ( array[i] > 0)
+    count++;
+}
+Console.WriteLine("Количество чисел больше нуля: " + count);
